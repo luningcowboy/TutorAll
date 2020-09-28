b699585f185fde5abf05b3c67b3afa59ba8ac44f
luning <luningcowboy@gmail.com>
2020-09-28 17:57:01 +0800

    add cs

---
+++ tutor-cs/t1.cs
@@ -0,0 +1,16 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 100;
+        double n2 = 1.01;
+        bool isFlag = false;
+        String name = "Hello"; 
+
+        Console.WriteLine($"n={n}");
+        Console.WriteLine($"n2={n2}");
+        Console.WriteLine($"isFlag={isFlag}");
+        Console.WriteLine($"name={name}");
+    }
+}
