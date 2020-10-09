e7afb142153dbf33d0b4ca91c495946076221216
luning <luningcowboy@gmail.com>
2020-10-09 14:16:46 +0800

    add t2

---
+++ tutor-cs/cores/t2.cs
@@ -0,0 +1,10 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+        Console.WriteLine("" + (a > b ? a + 10 : b + 10));
+    }
+}
