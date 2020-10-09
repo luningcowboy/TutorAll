a5e76eb46a0679cd1655a6b80de9ec717a041167
luning <luningcowboy@gmail.com>
2020-10-09 19:08:26 +0800

    add new code

---
+++ tutor-cs/cores/t6.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Progarm
+{
+    static void Main(string[] args)
+    {
+        const double PI = 3.14;
+        int r = 3;
+        Console.WriteLine(2 * PI * r);
+        Console.WriteLine(PI * r * r);
+    }
+}
