7cc6adcd398e17567d2efd5ed3b8c35e4ecc7f3d
luning <luningcowboy@gmail.com>
2020-10-12 21:55:50 +0800

    add new code

---
+++ tutor-cs/cores/t9.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write($"{i} x {j} = {i * j} \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
