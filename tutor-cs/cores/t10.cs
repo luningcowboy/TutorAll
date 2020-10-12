0bc55452256b2602d513e2e0fda1c0c41a50f966
luning <luningcowboy@gmail.com>
2020-10-12 22:00:40 +0800

    add new code

---
+++ tutor-cs/cores/t10.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for(int i = 1; i <= 3; i++)
+        {
+            for(int j = 1; j < 3 - i; j++)
+            {
+                Console.Write(" ");
+            }
+            for(int k = 1; k < 2 * i; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        for(int i = 2; i >= 1; i--)
+        {
+            for(int j = 1; j < 3 - 1; j++)
+            {
+                Console.Write(" ");
+            }
+            for(int k = 1; k < 2 * i - 1; k++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
