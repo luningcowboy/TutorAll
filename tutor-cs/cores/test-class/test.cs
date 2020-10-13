0c4f342276da451a8d3069e8962864d044047183
luning <luningcowboy@gmail.com>
2020-10-13 10:07:56 +0800

    add new code

---
+++ tutor-cs/cores/test-class/test.cs
@@ -0,0 +1,10 @@
+namespace Code_1
+{
+    class Test
+    {
+        private int id;
+        public readonly string name;
+        internal static int age;
+        private const string major = "计算机";
+    }
+}
